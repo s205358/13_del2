852dbbe0dc3d499cdcb526d31464bc8e5dc65e21
s205358 <s205358@student.dtu.dk>
2020-10-29 14:11:28 +0100

    Runnable BoardGame

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,4 +1,10 @@
 package Game;
 
 public class Main {
+    private static BoardGame game = new BoardGame();
+
+    public static void main(String[] args) {
+        game.setup();
+        game.run();
+    }
 }
