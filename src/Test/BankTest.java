3b73ab78d3dc60305a91336b51d9e065cad1effa
s205358 <s205358@student.dtu.dk>
2020-10-30 11:19:47 +0100

    Updated BankTest

--- src/Test/BankTest.java
+++ src/Test/BankTest.java
@@ -10,37 +10,30 @@ class BankTest {
 
     @Test
     void deposit() {
-        bank.setBalance(1000);
         bank.deposit(20);
         assertEquals(1020, bank.getBalance());
     }
 
     @Test
     void withdraw() {
-        bank.setBalance(1000);
         bank.withdraw(20);
         assertEquals(980, bank.getBalance());
-        bank.setBalance(1000);
-        bank.withdraw(1020);
+        bank.withdraw(1000);
         assertEquals(0, bank.getBalance());
     }
 
     @Test
     void updateBalance() {
-        bank.setBalance(1000);
         bank.updateBalance(20);
         assertEquals(1020, bank.getBalance());
-        bank.setBalance(1000);
         bank.updateBalance(-20);
-        assertEquals(980, bank.getBalance());
-        bank.setBalance(1000);
+        assertEquals(1000, bank.getBalance());
         bank.updateBalance(-1020);
         assertEquals(0, bank.getBalance());
     }
 
     @Test
     void testToString() {
-        bank.setBalance(1000);
         assertEquals("$1000", bank.toString());
     }
 }
